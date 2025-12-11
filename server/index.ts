31c4282c4f73b6669d48cac0fc020b9b7fa857f2
Jeganathan Digevan <digee12@gmail.com>
2025-12-11 18:55:42 +0530

    changes

--- server/index.ts
+++ server/index.ts
@@ -23,6 +23,9 @@ app.use(express.json({
 }));
 app.use(express.urlencoded({ extended: false }));
 
+// Serve attached assets
+app.use('/assets', express.static(path.resolve(process.cwd(), 'attached_assets')));
+
 app.use((req, res, next) => {
   const start = Date.now();
   const path = req.path;
